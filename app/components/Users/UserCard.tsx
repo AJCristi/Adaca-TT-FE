0328b08c4a74abc3df384db3522686c8e99cd652
AJCristi <60809557+AJCristi@users.noreply.github.com>
2025-08-14 17:55:32 +0800

    fix: added simple responsiveness

--- app/components/Users/UserCard.tsx
+++ app/components/Users/UserCard.tsx
@@ -27,12 +27,18 @@ interface Props {
 export default function UserCard({ user }: Props) {
     return (
         <Card title={user.name} subTitle={user.username}>
-            <p><strong>Email:</strong> {user.email}</p>
-            <p><strong>Phone:</strong> {user.phone}</p>
-            <p><strong>Website:</strong> {user.website}</p>
-            <p><strong>Company:</strong> {user.company.name} - {user.company.catchPhrase}</p>
-            <p><strong>Company Description:</strong> {user.company.bs}</p>
-            <p><strong>Address:</strong> {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}</p>
-        </Card>
+            <div className="p-grid p-dir-col md:p-dir-row p-gap-3">
+                <div className="p-col">
+                    <p><strong>Email:</strong> {user.email}</p>
+                    <p><strong>Phone:</strong> {user.phone}</p>
+                    <p><strong>Website:</strong> {user.website}</p>
+                </div>
+                <div className="p-col">
+                    <p><strong>Company:</strong> {user.company.name} - {user.company.catchPhrase}</p>
+                    <p><strong>Company Description:</strong> {user.company.bs}</p>
+                    <p><strong>Address:</strong> {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}</p>
+                </div>
+            </div>
+        </Card >
     );
 }
